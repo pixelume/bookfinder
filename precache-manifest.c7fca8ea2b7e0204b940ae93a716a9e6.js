d364c44fd446297f06c3a80adf23076a083d7abb
Pieter Wolmarans <pieter@pixelume.com>
2019-02-26 19:44:59 +0200

    Updates

---
+++ precache-manifest.c7fca8ea2b7e0204b940ae93a716a9e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e916f6c8d45a330d555c",
+    "url": "/bookfinder/static/css/main.d34ade57.chunk.css"
+  },
+  {
+    "revision": "e916f6c8d45a330d555c",
+    "url": "/bookfinder/static/js/main.e916f6c8.chunk.js"
+  },
+  {
+    "revision": "ea93145390ff8b0cfc07",
+    "url": "/bookfinder/static/js/runtime~main.ea931453.js"
+  },
+  {
+    "revision": "51b7303a7003daec6751",
+    "url": "/bookfinder/static/js/2.51b7303a.chunk.js"
+  },
+  {
+    "revision": "ec6ec0638ec249b6c2596f62e94e2a7b",
+    "url": "/bookfinder/static/media/noImg.ec6ec063.png"
+  },
+  {
+    "revision": "3ed30c1b11570abe24f7de4cf1128956",
+    "url": "/bookfinder/index.html"
+  }
+];
